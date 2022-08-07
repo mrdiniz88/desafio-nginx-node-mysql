3eb0e657022e824e287f0563451de36d0eb511e1
MrDiniz <mrdiniz@pop-os.localdomain>
2022-08-06 21:08:12 -0300

    fix: mysql server not found

--- app/src/index.js
+++ app/src/index.js
@@ -1,23 +1,38 @@
 const express = require("express");
+const { createConnection } = require("mysql");
 
-const { PrismaClient } = require("@prisma/client");
+const connection = createConnection({
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "desafio",
+});
 
-const prisma = new PrismaClient()
-const app = express()
+connection.connect(() => {
+  connection.query(`
+    CREATE TABLE IF NOT EXISTS PEOPLE(
+        ID INT NOT NULL AUTO_INCREMENT, 
+        NAME VARCHAR(30) NOT NULL,
+        PRIMARY KEY (ID)
+    )`);
+});
 
-app.get('/', async (req, res) => {
-    await prisma.people.create({
-        data: {
-            name: "Aluno Full"
-        }
-    })
-    const peoples = await prisma.people.findMany()
-    res.send(`
-    <h1>Full Cycle Rocks!</h1>
-    ${peoples.map((people) => {
-        return "-" + people.name + "<br/>" 
-    }).join('')}
-    `)
-})
+const app = express();
 
-app.listen(3000)
+app.get("/", async (req, res) => {
+  connection.query(
+    `INSERT INTO PEOPLE (name) VALUES ("Aluno Full Cycle")`,
+    (err, results, _) => {
+      connection.query(`SELECT * FROM PEOPLE`, (err, results, _) => {
+        res.send(`
+            <h1>Full Cycle Rocks!</h1>
+            ${results.map(({ NAME }) => {
+              return `- ${NAME}<br/>`;
+            }).join("")}
+            `);
+      });
+    }
+  );
+});
+
+app.listen(3000);
